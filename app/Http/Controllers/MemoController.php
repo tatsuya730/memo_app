303a8f3ba72b72934b4ace859c27dd7d8c15eef2
tatsuya730 <t.taniguchi730@gmail>
2023-10-29 10:31:42 +0900

    削除機能完成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -57,4 +57,12 @@ class MemoController extends Controller
 
         return redirect(route('memos.index'));
     }
+
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect(route("memos.index"));
+    }
 }
